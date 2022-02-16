a79767ac8428d92179956d3deedfd2feac640952
jjudykim <jjudykim0@gmail.com>
2022-02-16 22:57:54 +0900

    Ch 1_3 done.

---
+++ MyPlayground.playground/Pages/Ch1_3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+import Swift
+
+
+// 1. 상수와 변수 선언 ====================
+// 상수 선언 키워드 let
+// 변수 선언 키워드 var
+
+// 상수의 선언
+// let 이름: 타입 = 값
+let constant: String = "차후에 변경이 불가능한 변수 let"
+
+// 변수의 선언
+// var 이름 : 타입 = 값
+var variable: String = "차후에 변경이 가능한 변수 var"
+
+variable = "변수는 이렇게 차후에 다른 값을 할당할 수 있지만"
+//constant = "상수는 차후에 값을 변경할 수 없다" -> 오류 발생
+
+
+// 2. 상수를 선언 한 후 나중에 할당하기 =============
+let sum: Int
+let inputA: Int = 100
+let inputB: Int = 200
+
+// 선언 후 첫 할당
+sum = inputA + inputB
+
+// sum의 값을 바꿔본다면?
+// sum = 1 -> 오류 발생
+
+// 변수도 차후에 할당하는 것이 가능하다
+var nickName: String
+
+nickName = "jjudy"
+
+// 변수는 차후에 다시 다른 값을 할당해도 문제 없음!
+nickName = "쭈디"
