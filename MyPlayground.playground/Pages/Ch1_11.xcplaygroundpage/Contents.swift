b19445b875cdfcae3172b00a9c53b30b6dcbd580
jjudykim <imjubaaakim@gmail.com>
2022-02-20 23:25:25 +0900

    Ch1_11 옵셔널 표현 부분 1번까지

--- MyPlayground.playground/Pages/Ch1_11.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Ch1_11.xcplaygroundpage/Contents.swift
@@ -41,3 +41,24 @@ enum Optional<Wrapped>: ExpressibleByNilLiteral {
 // let optionalValue: Optional<Int> = nil
 // let optionalValue: Int? =nil
 
+
+// - 옵셔널 표현
+// 1) 느낌표(!)를 이용한 암시적 추출 옵셔널
+// Implicitly Unwrapped Optional
+var implicitlyUnwrappedOptionalValue: Int! = 100
+
+switch implicitlyUnwrappedOptionalValue {
+case .none:
+    print("This Optional variable is nil")
+case .some(let value):
+    print("Value is \(value)")
+}
+
+// 기존 변수처럼 사용 가능
+implicitlyUnwrappedOptionalValue = implicitlyUnwrappedOptionalValue + 1
+
+// nil 할당 가능
+implicitlyUnwrappedOptionalValue = nil
+
+// 잘못된 접근으로 인한 런타임 오류 발생
+//implicitlyUnwrappedOptionalValue = implicitlyUnwrappedOptionalValue + 1
